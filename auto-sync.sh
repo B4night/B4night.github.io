9e568b7a0a5191032ae745cf61ce634bafd0c15c
B4night <superchaovaction@163.com>
2024-02-05 14:53:24 +0800

    update 2024-02-05
    Signed-off-by: B4night <superchaovaction@163.com>

--- auto-sync.sh
+++ auto-sync.sh
@@ -9,15 +9,18 @@ npx mark-magic
 hexo_dir="./hexo/source/_posts"
 pushd $hexo_dir
 for md_file in *.md; do
-    file_name=$(cat $md_file | grep "title: " | cut -d ":" -f 2- | sed 's/"//g; s/ /_/g')
+    file_name=$(cat $md_file | grep "title: " | cut -d ":" -f 2-)
     timestamp_ms=$(cat $md_file | grep "date: " | awk -F ": " '{print $2}')
     timestamp_sec=$(($timestamp_ms / 1000))
     file_date=$(date -d "@$timestamp_sec" '+%Y-%m-%d')
+    sed -i "/$file_name\.md/d" $md_file
     sed -i '/updated:/d; /date:/d; /abbrlink:/d; /layout:/d' $md_file
     sed -i "2i date: $file_date" $md_file
     sed -i "2i author: B4night" $md_file
-    # mv "$md_file" "${file_name}.md"
-    mv $md_file ../../../src/posts/"${file_name}".md
+
+    file_name=$(echo $file_name | sed 's/"//g; s/ /_/g')
+    mv "$md_file" "${file_name}.md"
+    # mv $md_file ../../../src/posts/"${file_name}".md
     # pwd
 done
 
